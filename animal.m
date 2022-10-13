fbb1e67a168aafaf378134ff6536560176fe6707
XiaoxiaoZ <zxxzxxv@gmail.com>
2022-10-13 11:29:51 +0200

    snake robot

--- animal.m
+++ animal.m
@@ -1,26 +1,22 @@
-function distance = animal(omega1, omega2,omega3)
+function distance = animal(R1,R2,R3,PHI)
 %ANIMAL Summary of this function goes here
 %   Detailed explanation goes here ,, omega2, omega3, omega, phi, ar, ax, R1, R2, R3, X1, X2, X3
 
-omega1 = omega1;
-omega2 = omega2;
-omega3 = omega3;
-omega = ones(3,3)*0.5;
-omega(1,1)=0;
-omega(2,2)=0;
-omega(3,3)=0;
-phi = zeros(3,3);
-phi(1,2) = pi/2;
-phi(2,3) = pi;
-phi(3,2) = pi;
+omega1 = 0.6*pi;
+omega2 = 0.6*pi;
+omega3 = 0.6*pi;
+omega = ones(3,3)*2;
+phi = ones(3,3)*PHI;
 ar = 2;
 ax = 2;
-R1 = 4;
-R2 = 4;
-R3 = 4;
+R1 = R1;
+R2 = R2;
+R3 = R3;
 X1 = 0;
 X2 = 0;
 X3 = 0;
+%Robot unit length
+l=0.5;
 % omega2 = omega2;
 % omega3 = omega3;
 % omega = omega;
@@ -34,15 +30,16 @@ X3 = 0;
 % X2 = X2;
 % X3 = X3;
 options=simset('SrcWorkspace','current','DstWorkspace','base');
-simOut=sim('chain_CPG.slx', 10, options);
+simOut=sim('chain_CPG_2021a.slx', 10, options);
 %Run simulink simulation
 %simOut=sim('CPG.slx',10);
 %%Data extraction
 outVariable=get(simOut,'yout');
 distance = -simOut.distance.Data(end);
-disp(omega1)
-disp(omega2)
-disp(omega3)
-disp(distance)
+PHI
+R1
+R2
+R3
+distance
 end
 
--- parameters.m
+++ parameters.m
@@ -1,29 +1,28 @@
 clear
 clc
-omega1 = 0;
-omega2 = 0;
-omega3 = 0;
-omega = ones(3,3)*0.5;
-omega(1,1)=0;
-omega(2,2)=0;
-omega(3,3)=0;
-phi = zeros(3,3);
-phi(1,2) = pi/2;
-phi(2,3) = pi;
-phi(3,2) = pi;
+l=1;
+omega1 = 0.6*pi;
+omega2 = 0.6*pi;
+omega3 = 0.6*pi;
+omega = ones(3,3)*2;
+PHI = 0;
+phi = ones(3,3)*PHI;
 ar = 2;
 ax = 2;
-R1 = 4;
-R2 = 4;
-R3 = 4;
+R = 0.3;
+R1 = R;
+R2 = R;
+R3 = R;
 X1 = 0;
 X2 = 0;
 X3 = 0;
+%%
 rng default % For reproducibility
 options = optimoptions('ga','PlotFcn', @gaplotbestf);
-x = ga(@(x) animal(x(1),x(2),x(3)),3,options)
+x = ga(@(x) animal(x(1),x(2),x(3),x(4)),4,[],[],[],[],[0 0 0 0],[0.6 0.6 0.6 pi],[],options)
 %%
-omega1 = -64.1138;
-omega2 = -281.0089;
-omega3 = 93.1018;
-simOut=sim('chain_CPG.slx', 100);
+R1=0.5708;
+R2=0.5348;
+R3=0.3731;
+PHI=2.9214;
+simOut=sim('chain_CPG_2021a.slx', 100);
